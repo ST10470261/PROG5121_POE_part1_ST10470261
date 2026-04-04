a4d94bfb11fa001a33092656aaa5802b82f9f4cb
Nhlanhla Cindi <ST10470261@vcconnect.edu.za>
2026-04-04 02:37:24 +0200

    Initial commit : Implementation of login methods
    here is where I created a separate class in where I initially implemented different types of variables and methods required for the program

---
+++ src/main/java/com/mycompany/chatapp/ChatApp.java
@@ -0,0 +1,11 @@
+/*
+ *
+ */
+package com.mycompany.chatapp;
+
+public class ChatApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/com/mycompany/chatapp/LoginDetails.java
@@ -0,0 +1,61 @@
+package com.mycompany.chatapp;
+
+public class LoginDetails {
+    private String registeredUsername;
+    private String registeredPassword;
+    private String firstName;
+    private String lastName;
+    
+    public boolean checkUserName(String username){
+        return username.contains("_") && username.length() <= 5;
+    }
+    
+    public boolean checkPasswordComplexity(String password){
+        boolean hasUpper = false;
+        boolean hasNum = false;
+        boolean hasSpecial = false;
+        
+        if (password.length() < 8)return false;
+        
+        for(char c : password.toCharArray()){
+            if(Character.isUpperCase(c)) hasUpper = true;
+            if(Character.isDigit(c)) hasNum = true;
+            if(!Character.isLetterOrDigit(c)) hasSpecial = true;
+        }
+        return hasUpper && hasNum && hasSpecial;
+    }
+    
+    public boolean checkCellPhoneNumber(String cell){
+        return cell.matches("^\\+27[0-9]{9}$");
+    }
+    
+    public String registerUser(String user, String pass, String cell){
+        if(!checkUserName(user)){
+            return "Username is not correctly formatted; please ensure that your username contains an underscore and is no more than five characters in length.";
+        }
+        if(!checkPasswordComplexity(pass)){
+            return "Password is not correctly formatted; please ensure that the password contains atleast eight characters, a capital letter, a number, and a special character.";
+        }
+        if(!checkCellPhoneNumber(cell)){
+            return "Cell number is incorrectly formatted or does not contain an international code; please correct the number and  try again.";
+        }
+        
+        this.registeredUsername = user;
+        this.registeredPassword = pass;
+        return "Username succcessfully captured.\nPassword successfully captured.\nCell number successfully captured.";
+        
+    }
+    
+    public boolean loginUser(String user, String pass){
+        return user.equals(this.registeredUsername) && pass.equals(this.registeredPassword);
+    }
+    
+    public String returnLoginStatus(boolean success, String fName, String lName) {
+        if (success){
+            return "Welcome " + fName + "" + lName + " it is great to see you again."; 
+        }else{ 
+            return "UserName or password is incorrect, please try again.";
+        }
+    }
+    
+}
