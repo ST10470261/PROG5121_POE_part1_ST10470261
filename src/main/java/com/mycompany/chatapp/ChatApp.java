dd5722ff281623fbda8a40c913c1b2be39d1b777
Nhlanhla Cindi <ST10470261@vcconnect.edu.za>
2026-04-05 00:01:15 +0200

    Added comprehensive comments to Chat App class .

--- src/main/java/com/mycompany/chatapp/ChatApp.java
+++ src/main/java/com/mycompany/chatapp/ChatApp.java
@@ -1,14 +1,16 @@
-/*
- *
+/*Student name: Nhlanha Cindi
+ *ST number.  : ST10470261
+ *Module      : PROG5121
  */
 package com.mycompany.chatapp;
 import java.util.Scanner;
 public class ChatApp {
 
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);     //imported a scanner package so that the user is able to type their details directly into the console.
         LoginDetails login = new LoginDetails();
         
+        //This where we capture the user details/input.
         System.out.println("--- WELCOME TO CHATAPP REGISTRATION ---");
         System.out.print("Enter First Name: ");
         String fName = input.nextLine();
@@ -25,6 +27,7 @@ public class ChatApp {
         System.out.print("Enter SA Cell Phone Number (e.g. +27831234567): ");
         String cell = input.nextLine();
         
+        //This section manages the application flow through the usage of a "if" statement.
         String registrationStatus = login.registerUser(user, pass, cell);
         System.out.println("\n" + registrationStatus);
         
